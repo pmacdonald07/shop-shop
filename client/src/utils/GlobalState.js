75da4d241a0d5d54a15f7a8ddd6b6a180be05cdc
Patrick M <patrickmacdonald07@gmail.com>
2023-04-10 17:22:01 -0400

    set up global context

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,26 @@
+import React, { createContext, useContext } from 'react';
+import { useProductReducer } from './reducers';
+
+// Note to self: createContext will be used to instantiate a new Context object.
+// The more meaningful term we can use here is that we're using it to create the container
+// to hold our global state data and functionality so we can provide it throughout our app
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props }) => {
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    categories: [],
+    currentCategory: '',
+  });
+  // use this to confirm it works
+  console.log(state);
+  return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+  return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
