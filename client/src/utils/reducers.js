aca5b2fda722651db6f973d4b3fcc9aaea44f1ea
Patrick M <patrickmacdonald07@gmail.com>
2023-04-12 18:10:50 -0400

    added all cart functionality, including new tests and reducers, added cart components, updated global state

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -4,6 +4,12 @@ import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART,
 } from './actions';
 
 export const reducer = (state, actions) => {
@@ -26,6 +32,57 @@ export const reducer = (state, actions) => {
       return {
         ...state,
         currentCategory: actions.currentCategory,
+      };
+
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, actions.product],
+      };
+
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, ...actions.products],
+      };
+
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter(product => {
+        return product._id !== actions._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map(product => {
+          if (actions._id === product._id) {
+            product.purchaseQuantity = actions.purchaseQuantity;
+          }
+
+          return product;
+        }),
+      };
+
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: [],
+      };
+
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen,
       };
 
     // if it's none of these actions, do not update state at all and keep things the same!
